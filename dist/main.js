057fb53cef82dd494680eac27afa5e7617d4fc42
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-11-17 13:15:39 +0400

    перемешение по категориям

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";(()=>{let e=document.querySelectorAll(".header-email");console.log(e)})()})();
+(()=>{"use strict";document.querySelector(".top-menu").addEventListener("click",(e=>{let t=e.target;e.target.matches("ul>li>a")&&(e.preventDefault(),(e=>{let t=e.getAttribute("href").slice(1);document.getElementById(t).scrollIntoView({behavior:"smooth",block:"start"})})(t))}))})();
